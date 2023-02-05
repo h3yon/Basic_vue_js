3eb430e3187e82a7d51b9351ebfd87d1540bdd33
h3yon <khykhy1006@gmail.com>
2023-02-05 16:47:57 +0900

    feat) checkbox data binding

---
+++ project/src/views/1_databinding/DataBindingCheckboxView.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <div>
+      <input
+        type="checkbox"
+        name=""
+        id="html"
+        value="HTML"
+        v-model="favoriteLanguage"
+      />
+      <label for="html">HTML</label>
+    </div>
+    <div>
+      <input
+        type="checkbox"
+        name=""
+        id="css"
+        value="CSS"
+        v-model="favoriteLanguage"
+      />
+      <label for="css">CSS</label>
+    </div>
+    <div>
+      <input
+        type="checkbox"
+        name=""
+        id="js"
+        value="JS"
+        v-model="favoriteLanguage"
+      />
+      <label for="js">JS</label>
+    </div>
+    <div>선택한 지역: {{ favoriteLanguage }}</div>
+  </div>
+</template>
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      favoriteLanguage: []
+    }
+  },
+  beforeCreate() {},
+  created() {},
+  beforeMount() {},
+  mounted() {},
+  beforeUpdate() {},
+  updated() {},
+  beforeUnmount() {},
+  unmounted() {},
+  methods: {}
+}
+</script>
